768052edb2afecee646da543ea4e72f441fb593c
15000rpm <59537990+Meteliza@users.noreply.github.com>
2021-11-21 03:56:27 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,142 @@
+/* global config csv2geojson turf Assembly $ */
+'use strict';
+
+mapboxgl.accessToken = config.accessToken;
+const columnHeaders = config.sideBarInfo;
+
+let geojsonData = {};
+const filteredGeojson = {
+  type: 'FeatureCollection',
+  features: [],
+};
+
+const map = new mapboxgl.Map({
+  container: 'map',
+  style: config.style,
+  center: config.center,
+  zoom: config.zoom,
+  transformRequest: transformRequest,
+});
+
+function flyToLocation(currentFeature) {
+  map.flyTo({
+    center: currentFeature,
+    zoom: 11,
+  });
+}
+
+function createPopup(currentFeature) {
+  const popups = document.getElementsByClassName('mapboxgl-popup');
+  /** Check if there is already a popup on the map and if so, remove it */
+  if (popups[0]) popups[0].remove();
+  new mapboxgl.Popup({ closeOnClick: true })
+    .setLngLat(currentFeature.geometry.coordinates)
+    .setHTML('<h3>' + currentFeature.properties[config.popupInfo] + '</h3>')
+    .addTo(map);
+}
+
+function buildLocationList(locationData) {
+  /* Add a new listing section to the sidebar. */
+  const listings = document.getElementById('listings');
+  listings.innerHTML = '';
+  locationData.features.forEach((location, i) => {
+    const prop = location.properties;
+
+    const listing = listings.appendChild(document.createElement('div'));
+    /* Assign a unique `id` to the listing. */
+    listing.id = 'listing-' + prop.id;
+
+    /* Assign the `item` class to each listing for styling. */
+    listing.className = 'item';
+
+    /* Add the link to the individual listing created above. */
+    const link = listing.appendChild(document.createElement('button'));
+    link.className = 'title';
+    link.id = 'link-' + prop.id;
+    link.innerHTML =
+      '<p style="line-height: 1.25">' + prop[columnHeaders[0]] + '</p>';
+
+    /* Add details to the individual listing. */
+    const details = listing.appendChild(document.createElement('div'));
+    details.className = 'content';
+
+    for (let i = 1; i < columnHeaders.length; i++) {
+      const div = document.createElement('div');
+      div.innerText += prop[columnHeaders[i]];
+      div.className;
+      details.appendChild(div);
+    }
+
+    link.addEventListener('click', function () {
+      const clickedListing = location.geometry.coordinates;
+      flyToLocation(clickedListing);
+      createPopup(location);
+
+      const activeItem = document.getElementsByClassName('active');
+      if (activeItem[0]) {
+        activeItem[0].classList.remove('active');
+      }
+      this.parentNode.classList.add('active');
+
+      const divList = document.querySelectorAll('.content');
+      const divCount = divList.length;
+      for (i = 0; i < divCount; i++) {
+        divList[i].style.maxHeight = null;
+      }
+
+      for (let i = 0; i < geojsonData.features.length; i++) {
+        this.parentNode.classList.remove('active');
+        this.classList.toggle('active');
+        const content = this.nextElementSibling;
+        if (content.style.maxHeight) {
+          content.style.maxHeight = null;
+        } else {
+          content.style.maxHeight = content.scrollHeight + 'px';
+        }
+      }
+    });
+  });
+}
+
+// Build dropdown list function
+// title - the name or 'category' of the selection e.g. 'Languages: '
+// defaultValue - the default option for the dropdown list
+// listItems - the array of filter items
+
+function buildDropDownList(title, listItems) {
+  const filtersDiv = document.getElementById('filters');
+  const mainDiv = document.createElement('div');
+  const filterTitle = document.createElement('h3');
+  filterTitle.innerText = title;
+  filterTitle.classList.add('py12', 'txt-bold');
+  mainDiv.appendChild(filterTitle);
+
+  const selectContainer = document.createElement('div');
+  selectContainer.classList.add('select-container', 'center');
+
+  const dropDown = document.createElement('select');
+  dropDown.classList.add('select', 'filter-option');
+
+  const selectArrow = document.createElement('div');
+  selectArrow.classList.add('select-arrow');
+
+  const firstOption = document.createElement('option');
+
+  dropDown.appendChild(firstOption);
+  selectContainer.appendChild(dropDown);
+  selectContainer.appendChild(selectArrow);
+  mainDiv.appendChild(selectContainer);
+
+  for (let i = 0; i < listItems.length; i++) {
+    const opt = listItems[i];
+    const el1 = document.createElement('option');
+    el1.textContent = opt;
+    el1.value = opt;
+    dropDown.appendChild(el1);
+  }
+  filtersDiv.appendChild(mainDiv);
+}
+
 // Build checkbox function
 // title - the name or 'category' of the selection e.g. 'Languages: '
 // listItems - the array of filter items
@@ -224,3 +363,152 @@ createFilterObject(config.filters);
 applyFilters();
 filters(config.filters);
 removeFiltersButton();
+
+const geocoder = new MapboxGeocoder({
+  accessToken: mapboxgl.accessToken, // Set the access token
+  mapboxgl: mapboxgl, // Set the mapbox-gl instance
+  marker: true, // Use the geocoder's default marker style
+  zoom: 11,
+});
+
+function sortByDistance(selectedPoint) {
+  const options = { units: 'miles' };
+  let data;
+  if (filteredGeojson.features.length > 0) {
+    data = filteredGeojson;
+  } else {
+    data = geojsonData;
+  }
+  data.features.forEach((data) => {
+    Object.defineProperty(data.properties, 'distance', {
+      value: turf.distance(selectedPoint, data.geometry, options),
+      writable: true,
+      enumerable: true,
+      configurable: true,
+    });
+  });
+
+  data.features.sort((a, b) => {
+    if (a.properties.distance > b.properties.distance) {
+      return 1;
+    }
+    if (a.properties.distance < b.properties.distance) {
+      return -1;
+    }
+    return 0; // a must be equal to b
+  });
+  const listings = document.getElementById('listings');
+  while (listings.firstChild) {
+    listings.removeChild(listings.firstChild);
+  }
+  buildLocationList(data);
+}
+
+geocoder.on('result', (ev) => {
+  const searchResult = ev.result.geometry;
+  sortByDistance(searchResult);
+});
+
+map.on('load', () => {
+  map.addControl(geocoder, 'top-right');
+
+  // csv2geojson - following the Sheet Mapper tutorial https://www.mapbox.com/impact-tools/sheet-mapper
+  console.log('loaded');
+  $(document).ready(() => {
+    console.log('ready');
+    $.ajax({
+      type: 'GET',
+      url: config.CSV,
+      dataType: 'text',
+      success: function (csvData) {
+        makeGeoJSON(csvData);
+      },
+      error: function (request, status, error) {
+        console.log(request);
+        console.log(status);
+        console.log(error);
+      },
+    });
+  });
+
+  function makeGeoJSON(csvData) {
+    csv2geojson.csv2geojson(
+      csvData,
+      {
+        latfield: 'Latitude',
+        lonfield: 'Longitude',
+        delimiter: ',',
+      },
+      (err, data) => {
+        data.features.forEach((data, i) => {
+          data.properties.id = i;
+        });
+
+        geojsonData = data;
+        // Add the the layer to the map
+        map.addLayer({
+          id: 'locationData',
+          type: 'circle',
+          source: {
+            type: 'geojson',
+            data: geojsonData,
+          },
+          paint: {
+            'circle-radius': 5, // size of circles
+            'circle-color': '#3D2E5D', // color of circles
+            'circle-stroke-color': 'white',
+            'circle-stroke-width': 1,
+            'circle-opacity': 0.7,
+          },
+        });
+      },
+    );
+
+    map.on('click', 'locationData', (e) => {
+      const features = map.queryRenderedFeatures(e.point, {
+        layers: ['locationData'],
+      });
+      const clickedPoint = features[0].geometry.coordinates;
+      flyToLocation(clickedPoint);
+      sortByDistance(clickedPoint);
+      createPopup(features[0]);
+    });
+
+    map.on('mouseenter', 'locationData', () => {
+      map.getCanvas().style.cursor = 'pointer';
+    });
+
+    map.on('mouseleave', 'locationData', () => {
+      map.getCanvas().style.cursor = '';
+    });
+    buildLocationList(geojsonData);
+  }
+});
+
+// Modal - popup for filtering results
+const filterResults = document.getElementById('filterResults');
+const exitButton = document.getElementById('exitButton');
+const modal = document.getElementById('modal');
+
+filterResults.addEventListener('click', () => {
+  modal.classList.remove('hide-visually');
+  modal.classList.add('z5');
+});
+
+exitButton.addEventListener('click', () => {
+  modal.classList.add('hide-visually');
+});
+
+const title = document.getElementById('title');
+title.innerText = config.title;
+const description = document.getElementById('description');
+description.innerText = config.description;
+
+function transformRequest(url) {
+  const isMapboxRequest =
+    url.slice(8, 22) === 'api.mapbox.com' ||
+    url.slice(10, 26) === 'tiles.mapbox.com';
+  return {
+    url: isMapboxRequest ? url.replace('?', '?pluginName=finder&') : url,
+  };
+}
