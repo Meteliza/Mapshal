66e6e1f4c6e4e937f9e256b39bd24555dee8fc61
15000rpm <59537990+Meteliza@users.noreply.github.com>
2021-11-21 03:30:54 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,12 @@
+const config = {
+		sideBarInfo: ['Name', 'Address'],
+  	filters: [
+    {
+      type: 'checkbox',
+      title: '人物: ',
+      columnHeader: '人物', // Case sensitive - must match spreadsheet entry
+      listItems: ['华西列夫斯基', '华西列夫斯基父亲'], // Case sensitive - must match spreadsheet entry; This will take up to six inputs but is best used with a maximum of three;
+    },
+  ],
+};
+    
