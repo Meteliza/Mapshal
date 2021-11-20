616d94840c38f11d5f806940fbc2d6cff9975ce4
15000rpm <59537990+Meteliza@users.noreply.github.com>
2021-11-21 03:53:56 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,226 +1,74 @@
-// Build checkbox function
-// title - the name or 'category' of the selection e.g. 'Languages: '
-// listItems - the array of filter items
-// To DO: Clean up code - for every third checkbox, create a div and append new checkboxes to it
+'use strict';
 
-function buildCheckbox(title, listItems) {
-  const filtersDiv = document.getElementById('filters');
-  const mainDiv = document.createElement('div');
-  const filterTitle = document.createElement('div');
-  const formatcontainer = document.createElement('div');
-  filterTitle.classList.add('center', 'flex-parent', 'py12', 'txt-bold');
-  formatcontainer.classList.add(
-    'center',
-    'flex-parent',
-    'flex-parent--column',
-    'px3',
-    'flex-parent--space-between-main',
-  );
-  const secondLine = document.createElement('div');
-  secondLine.classList.add(
-    'center',
-    'flex-parent',
-    'py12',
-    'px3',
-    'flex-parent--space-between-main',
-  );
-  filterTitle.innerText = title;
-  mainDiv.appendChild(filterTitle);
-  mainDiv.appendChild(formatcontainer);
-
-  for (let i = 0; i < listItems.length; i++) {
-    const container = document.createElement('label');
-
-    container.classList.add('checkbox-container');
-
-    const input = document.createElement('input');
-    input.classList.add('px12', 'filter-option');
-    input.setAttribute('type', 'checkbox');
-    input.setAttribute('id', listItems[i]);
-    input.setAttribute('value', listItems[i]);
-
-    const checkboxDiv = document.createElement('div');
-    const inputValue = document.createElement('p');
-    inputValue.innerText = listItems[i];
-    checkboxDiv.classList.add('checkbox', 'mr6');
-    checkboxDiv.appendChild(Assembly.createIcon('check'));
-
-    container.appendChild(input);
-    container.appendChild(checkboxDiv);
-    container.appendChild(inputValue);
-
-    formatcontainer.appendChild(container);
-  }
-  filtersDiv.appendChild(mainDiv);
-}
-
-const selectFilters = [];
-const checkboxFilters = [];
-
-function createFilterObject(filterSettings) {
-  filterSettings.forEach((filter) => {
-    if (filter.type === 'checkbox') {
-      const keyValues = {};
-      Object.assign(keyValues, {
-        header: filter.columnHeader,
-        value: filter.listItems,
-      });
-      checkboxFilters.push(keyValues);
-    }
-    if (filter.type === 'dropdown') {
-      const keyValues = {};
-      Object.assign(keyValues, {
-        header: filter.columnHeader,
-        value: filter.listItems,
-      });
-      selectFilters.push(keyValues);
-    }
-  });
-}
-
-function applyFilters() {
-  const filterForm = document.getElementById('filters');
-
-  filterForm.addEventListener('change', function () {
-    const filterOptionHTML = this.getElementsByClassName('filter-option');
-    const filterOption = [].slice.call(filterOptionHTML);
-
-    const geojSelectFilters = [];
-    const geojCheckboxFilters = [];
-
-    filteredGeojson.features = [];
-    // const filteredFeatures = [];
-    // filteredGeojson.features = [];
-
-    filterOption.forEach((filter) => {
-      if (filter.type === 'checkbox' && filter.checked) {
-        checkboxFilters.forEach((objs) => {
-          Object.entries(objs).forEach(([, value]) => {
-            if (value.includes(filter.value)) {
-              const geojFilter = [objs.header, filter.value];
-              geojCheckboxFilters.push(geojFilter);
-            }
-          });
-        });
-      }
-      if (filter.type === 'select-one' && filter.value) {
-        selectFilters.forEach((objs) => {
-          Object.entries(objs).forEach(([, value]) => {
-            if (value.includes(filter.value)) {
-              const geojFilter = [objs.header, filter.value];
-              geojSelectFilters.push(geojFilter);
-            }
-          });
-        });
-      }
-    });
-
-    if (geojCheckboxFilters.length === 0 && geojSelectFilters.length === 0) {
-      geojsonData.features.forEach((feature) => {
-        filteredGeojson.features.push(feature);
-      });
-    } else if (geojCheckboxFilters.length > 0) {
-      geojCheckboxFilters.forEach((filter) => {
-        geojsonData.features.forEach((feature) => {
-          if (feature.properties[filter[0]].includes(filter[1])) {
-            if (
-              filteredGeojson.features.filter(
-                (f) => f.properties.id === feature.properties.id,
-              ).length === 0
-            ) {
-              filteredGeojson.features.push(feature);
-            }
-          }
-        });
-      });
-      if (geojSelectFilters.length > 0) {
-        const removeIds = [];
-        filteredGeojson.features.forEach((feature) => {
-          let selected = true;
-          geojSelectFilters.forEach((filter) => {
-            if (
-              feature.properties[filter[0]].indexOf(filter[1]) < 0 &&
-              selected === true
-            ) {
-              selected = false;
-              removeIds.push(feature.properties.id);
-            } else if (selected === false) {
-              removeIds.push(feature.properties.id);
-            }
-          });
-        });
-        removeIds.forEach((id) => {
-          const idx = filteredGeojson.features.findIndex(
-            (f) => f.properties.id === id,
-          );
-          filteredGeojson.features.splice(idx, 1);
-        });
-      }
-    } else {
-      geojsonData.features.forEach((feature) => {
-        let selected = true;
-        geojSelectFilters.forEach((filter) => {
-          if (
-            !feature.properties[filter[0]].includes(filter[1]) &&
-            selected === true
-          ) {
-            selected = false;
-          }
-        });
-        if (
-          selected === true &&
-          filteredGeojson.features.filter(
-            (f) => f.properties.id === feature.properties.id,
-          ).length === 0
-        ) {
-          filteredGeojson.features.push(feature);
-        }
-      });
-    }
-
-    map.getSource('locationData').setData(filteredGeojson);
-    buildLocationList(filteredGeojson);
-  });
-}
-
-function filters(filterSettings) {
-  filterSettings.forEach((filter) => {
-    if (filter.type === 'checkbox') {
-      buildCheckbox(filter.title, filter.listItems);
-    } else if (filter.type === 'dropdown') {
-      buildDropDownList(filter.title, filter.listItems);
-    }
-  });
-}
-
-function removeFilters() {
-  const input = document.getElementsByTagName('input');
-  const select = document.getElementsByTagName('select');
-  const selectOption = [].slice.call(select);
-  const checkboxOption = [].slice.call(input);
-  filteredGeojson.features = [];
-  checkboxOption.forEach((checkbox) => {
-    if (checkbox.type === 'checkbox' && checkbox.checked === true) {
-      checkbox.checked = false;
-    }
-  });
-
-  selectOption.forEach((option) => {
-    option.selectedIndex = 0;
-  });
-
-  map.getSource('locationData').setData(geojsonData);
-  buildLocationList(geojsonData);
-}
-
-function removeFiltersButton() {
-  const removeFilter = document.getElementById('removeFilters');
-  removeFilter.addEventListener('click', () => {
-    removeFilters();
-  });
-}
-
-createFilterObject(config.filters);
-applyFilters();
-filters(config.filters);
-removeFiltersButton();
+// eslint-disable-next-line no-unused-vars
+const config = {
+  style: 'mapbox://styles/mapbox/light-v10',
+  accessToken:
+    'pk.eyJ1IjoibWFwYm94LWNvbW11bml0eSIsImEiOiJja2tkN21jcjAwMG51MnBxdHAxemdueGpzIn0.e0IzLkytGq4pcGGieP8KNA',
+  CSV: './Sample_Data.csv',
+  center: [-120.234, 47.398],
+  zoom: 6,
+  title: 'Replace with your title',
+  description:
+    'Replace with information about your application. Ex. You can search by address to sort the list below by distance. You can also filter the list by language support options, which days a location is open, and whether they have devices to use to complete the survey by phone or online.',
+  sideBarInfo: ['Location_Name', 'Address', 'Phone'],
+  popupInfo: ['Location_Name'],
+  filters: [
+    {
+      type: 'dropdown',
+      title: 'Languages supported: ',
+      columnHeader: 'Languages',
+      listItems: [
+        'Amharic',
+        'ASL',
+        'Cambodian',
+        'Chinese',
+        'Danish',
+        'English',
+        'French',
+        'German',
+        'Greek',
+        'Hindi',
+        'Italian',
+        'Japanese',
+        'Korean',
+        'Language Line Services',
+        'Norwegian',
+        'Oriya',
+        'Portuguese',
+        'Punjabi',
+        'Russian',
+        'Somali',
+        'Spanish',
+        'Swedish',
+        'Tagalog',
+        'Thai',
+        'Tigrinya',
+        'Tongan',
+        'Vietnamese',
+        'Ukranian',
+      ],
+    },
+    {
+      type: 'checkbox',
+      title: 'Devices available: ',
+      columnHeader: 'Devices_available', // Case sensitive - must match spreadsheet entry
+      listItems: ['Computer', 'Wi-Fi', 'Adaptive Laptops'], // Case sensitive - must match spreadsheet entry; This will take up to six inputs but is best used with a maximum of three;
+    },
+    {
+      type: 'dropdown',
+      title: 'Clients: ',
+      columnHeader: 'Clients',
+      listItems: [
+        'Adults',
+        'Disabled',
+        'Homeless',
+        'Immigrants/Refugees',
+        'Low Income',
+        'Seniors',
+        'Youth: Pre-teen',
+        'Youth: Teen',
+      ],
+    },
+  ],
+};
